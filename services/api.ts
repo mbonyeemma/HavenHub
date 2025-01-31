2a1545287c024003d5b4d3c52a2fb2bec1d5f4e1
Andy <andy@inisev.com>
2025-01-31 18:12:09 +0300

    latest

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3005/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.bavana.site/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
